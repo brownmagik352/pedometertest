306c2ccd4a1f958829d91b72cd69eaf76f657502
Apurv Suman <sumanicus@gmail.com>
2018-04-04 22:35:16 -0700

    smooth signal graph added

--- app/src/main/java/com/example/brownmagik352/pedometertest/MainActivity.java
+++ app/src/main/java/com/example/brownmagik352/pedometertest/MainActivity.java
@@ -25,10 +25,21 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private float _rawAccelValues[] = new float[3];
 
     // graphview stuff
-    private LineGraphSeries<DataPoint> _seriesX;
-    private LineGraphSeries<DataPoint> _seriesY;
-    private LineGraphSeries<DataPoint> _seriesZ;
+    private LineGraphSeries<DataPoint> _rawX;
+    private LineGraphSeries<DataPoint> _rawY;
+    private LineGraphSeries<DataPoint> _rawZ;
+    private LineGraphSeries<DataPoint> _smoothX;
+    private LineGraphSeries<DataPoint> _smoothY;
+    private LineGraphSeries<DataPoint> _smoothZ;
     private double graphLastXValue = 1d;
+
+    // smoothing accelerometer signal stuff
+    private static int MAX_ACCEL_VALUE = 30;
+    private static int SMOOTHING_WINDOW_SIZE = 20;
+    private float _accelValueHistory[][] = new float[3][SMOOTHING_WINDOW_SIZE];
+    private float _runningAccelTotal[] = new float[3];
+    private float _curAccelAvg[] = new float[3];
+    private int _curReadIndex = 0;
 
 
     @Override
@@ -48,49 +59,83 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         // See also: https://developer.android.com/reference/android/hardware/SensorManager.html#SENSOR_DELAY_UI
         _sensorManager.registerListener(this, _accelSensor, SensorManager.SENSOR_DELAY_GAME);
 
-        // graphview test
-        GraphView graph = (GraphView) findViewById(R.id.graphRaw);
-        _seriesX = new LineGraphSeries<>(new DataPoint[] {
+        // raw graph
+        GraphView graphRaw = (GraphView) findViewById(R.id.graphRaw);
+        _rawX = new LineGraphSeries<>(new DataPoint[] {
                 new DataPoint(0.0,0.0)
         });
-        _seriesY = new LineGraphSeries<>(new DataPoint[] {
+        _rawY = new LineGraphSeries<>(new DataPoint[] {
                 new DataPoint(0.0,0.0)
         });
-        _seriesZ = new LineGraphSeries<>(new DataPoint[] {
+        _rawZ = new LineGraphSeries<>(new DataPoint[] {
                 new DataPoint(0.0,0.0)
         });
-        graph.addSeries(_seriesX);
-        graph.addSeries(_seriesY);
-        graph.addSeries(_seriesZ);
-        graph.getViewport().setXAxisBoundsManual(true);
-        graph.getViewport().setMinX(0);
-        graph.getViewport().setMaxX(10);
+        graphRaw.addSeries(_rawX);
+        graphRaw.addSeries(_rawY);
+        graphRaw.addSeries(_rawZ);
+        graphRaw.getViewport().setXAxisBoundsManual(true);
+        graphRaw.getViewport().setMinX(0);
+        graphRaw.getViewport().setMaxX(10);
+
+        // smooth graph
+        GraphView graphSmooth = (GraphView) findViewById(R.id.graphSmooth);
+        _smoothX = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0.0,0.0)
+        });
+        _smoothY = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0.0,0.0)
+        });
+        _smoothZ = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0.0,0.0)
+        });
+        graphSmooth.addSeries(_smoothX);
+        graphSmooth.addSeries(_smoothY);
+        graphSmooth.addSeries(_smoothZ);
+        graphSmooth.getViewport().setXAxisBoundsManual(true);
+        graphSmooth.getViewport().setMinX(0);
+        graphSmooth.getViewport().setMaxX(10);
     }
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
         switch(sensorEvent.sensor.getType()) {
             case Sensor.TYPE_ACCELEROMETER:
+
+                // pull raw values
                 _rawAccelValues[0] = sensorEvent.values[0];
                 _rawAccelValues[1] = sensorEvent.values[1];
                 _rawAccelValues[2] = sensorEvent.values[2];
 
+                // Smoothing algorithm adapted from: https://www.arduino.cc/en/Tutorial/Smoothing
+                for (int i = 0; i < 3; i++) {
+                    _runningAccelTotal[i] = _runningAccelTotal[i] - _accelValueHistory[i][_curReadIndex];
+                    _accelValueHistory[i][_curReadIndex] = _rawAccelValues[i];
+                    _runningAccelTotal[i] = _runningAccelTotal[i] + _accelValueHistory[i][_curReadIndex];
+                    _curAccelAvg[i] = _runningAccelTotal[i] / SMOOTHING_WINDOW_SIZE;
+                }
 
-//                Log.i("RAW0", Float.toString(_rawAccelValues[0]));
-//                Log.i("RAW1", Float.toString(_rawAccelValues[1]));
-//                Log.i("RAW2", Float.toString(_rawAccelValues[2]));
+                _curReadIndex++;
+                if(_curReadIndex >= SMOOTHING_WINDOW_SIZE){
+                    _curReadIndex = 0;
+                }
 
+                // graph updates
+                graphLastXValue += 1d;
+                _rawX.appendData(new DataPoint(graphLastXValue, _rawAccelValues[0]), true, 10);
+                _rawY.appendData(new DataPoint(graphLastXValue, _rawAccelValues[1]), true, 10);
+                _rawZ.appendData(new DataPoint(graphLastXValue, _rawAccelValues[2]), true, 10);
+                _smoothX.appendData(new DataPoint(graphLastXValue, _curAccelAvg[0]), true, 10);
+                _smoothY.appendData(new DataPoint(graphLastXValue, _curAccelAvg[1]), true, 10);
+                _smoothZ.appendData(new DataPoint(graphLastXValue, _curAccelAvg[2]), true, 10);
+
+                // debug visualization
                 TextView rawX = (TextView) findViewById(R.id.rawX);
                 TextView rawY = (TextView) findViewById(R.id.rawY);
                 TextView rawZ = (TextView) findViewById(R.id.rawZ);
-                rawX.setText(String.format("rawX: %f",  _rawAccelValues[0]));
-                rawY.setText(String.format("rawY: %f",  _rawAccelValues[1]));
-                rawZ.setText(String.format("rawZ: %f",  _rawAccelValues[2]));
+                rawX.setText(String.format("rawX: %f\t\t\tsmoothX: %f",  _rawAccelValues[0], _curAccelAvg[0]));
+                rawY.setText(String.format("rawY: %f\t\t\tsmoothY: %f",  _rawAccelValues[1], _curAccelAvg[1]));
+                rawZ.setText(String.format("rawZ: %f\t\t\tsmoothZ: %f",  _rawAccelValues[2], _curAccelAvg[2]));
 
-                graphLastXValue += 1d;
-                _seriesX.appendData(new DataPoint(graphLastXValue, _rawAccelValues[0]), true, 10);
-                _seriesY.appendData(new DataPoint(graphLastXValue, _rawAccelValues[1]), true, 10);
-                _seriesZ.appendData(new DataPoint(graphLastXValue, _rawAccelValues[2]), true, 10);
 
         }
     }
